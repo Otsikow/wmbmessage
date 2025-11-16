94ff409401aefafa37c7fdb25b9d030c9661bda8
Otsikow <eric777arthur@gmail.com>
2025-11-16 21:11:51 +0000

    Add retryable app-level error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import ErrorBoundary from "./components/ErrorBoundary.tsx";
+import AppRoot from "./AppRoot";
 import "./index.css";
 
 // Register service worker for offline support
@@ -41,8 +40,6 @@ if ('serviceWorker' in navigator) {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <AppRoot />
   </StrictMode>
 );
